258eeefa3bede7098781d663a0ef3f5115d7a7f5
innocentdavid <paulinnocent04@gmail.com>
2023-09-02 20:24:19 +0100

    added check active user

--- src/pages/admin/Retention.jsx
+++ src/pages/admin/Retention.jsx
@@ -6,8 +6,9 @@ import { Link, useNavigate } from 'react-router-dom'
 import { countDays } from '../../helpers'
 import copy from 'copy-to-clipboard';
 import { ChangeStatusModal, calculateLast7DaysGrowth, statuses } from './ManagePage'
-import { FaInstagram } from 'react-icons/fa'
+import { FaInstagram, FaTimes } from 'react-icons/fa'
 import { LOGO } from '../../config'
+import axios from 'axios'
 
 export default function Retention() {
   const navigate = useNavigate();
@@ -21,6 +22,7 @@ export default function Retention() {
   const [refreshUsers, setRefreshUsers] = useState(false)
   const [loading, setLoading] = useState(false)
   const [message, setMessage] = useState({ sectionName: '', value: '' })
+  const [showCheckActiveUsersModal, setSetshowCheckActiveUsersModal] = useState(false)
 
   // verity user
   useEffect(() => {
@@ -60,6 +62,7 @@ export default function Retention() {
     fetch()
   }, [sectionName, refreshUsers])
 
+  // growthDifference
   useEffect(() => {
     if (users.length > 0) {
       users.forEach(async user => {
@@ -95,6 +98,13 @@ export default function Retention() {
 
   return (
     <div className="font-MontserratRegular max-w-[1600px] mx-auto">
+      {showCheckActiveUsersModal && <div className="fixed top-0 left-0 w-full h-screen bg-transparent z-[99999]">
+        <div className="absolute top-0 left-0 h-full w-full z-[2] bg-black/50 cursor-pointer" onClick={() => { setSetshowCheckActiveUsersModal(false) }}></div>
+        <div className="fixed top-0 left-1/2 -translate-x-1/2 h-full min-w-[320px] z-[3] bg-white shadow-2xl rounded-2xl p-4">
+          <CheckActiveUsers />
+        </div>
+      </div>}
+
       {showChargebee && <Chargebee k={selectedUser?.id} user={selectedUser} setShowChargebee={setShowChargebee} />}
       <Header
         setUsers={setUsers}
@@ -102,6 +112,12 @@ export default function Retention() {
         setSearchTerm={setSearchTerm}
         setLoading={setLoading}
       />
+
+      <div className="mt-[30px] h-[59px] w-fit rounded-[10px] bg-[#000000] text-[25px] font-bold font-MontserratBold text-white px-4 flex justify-center items-center relative">
+        <div className="flex items-center justify-center capitalize cursor-pointer select-none"
+          onClick={() => { setSetshowCheckActiveUsersModal(true) }}>Check Active users
+        </div>
+      </div>
 
       <div className="mt-[30px] h-[82px] w-full rounded-[10px] border shadow-[0px_0px_5px_0px_#E7E7E7] px-5 flex items-center gap-2">
         {statuses.map(status => {
@@ -136,13 +152,13 @@ export default function Retention() {
         </thead>
 
         <tbody>
-          {users.map(user => {
+          {users.map((user, index) => {
             if (!user) {
               return ("Loading")
             }
 
             return (
-              <tr key={`${user?.username}_row`} className='rounded-[10px] bg-[#F8F8F8] h-[64px] w-full'>
+              <tr key={`${user?.username}_${index + 1}_row`} className='rounded-[10px] bg-[#F8F8F8] h-[64px] w-full'>
                 <td>
                   <img src={user?.profile_pic_url} alt="" className="w-[30px] h-[30px] min-w-[30px] min-h-[30px] rounded-full bg-black ml-4" />
                 </td>
@@ -210,4 +226,200 @@ export default function Retention() {
       </table>
     </div>
   )
+}
+
+const CheckActiveUsers = () => {
+  const [loading, setLoading] = useState(true)
+  const [users, setUsers] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [message, setMessage] = useState({ sectionName: '', value: '' })
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true)
+      const { data, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq("status", 'active'.toLocaleLowerCase())
+        .limit(3000)
+      error && console.log(error);
+      if (error) return;
+
+      setUsers(data);
+      setLoading(false)
+
+      var count = 0;
+      while (count < data.length) {
+        const user = data[count];
+        const chargebee_customer_id = user.chargebee_customer_id;
+        const baseUrl = process.env.REACT_APP_BASE_URL
+        if (chargebee_customer_id) {
+          let subscription = await axios.post(`${baseUrl}/api/subscription_list`,
+            { customer_id: chargebee_customer_id })
+            .then((response) => response.data)
+          const status = (subscription?.status === 'active' && subscription?.due_invoices_count > 0) ?
+            `Invoice Due ( ${subscription?.due_invoices_count} )` : subscription?.status
+          const el = document.getElementById(`chargebee_status_${user.username}`)
+          if (el) {
+            el.textContent = status;
+          }
+          // newList.push({ ...data, sub_status: status })
+        }
+        count++
+      }
+    }
+    fetch()
+  }, [])
+
+  const hangleSearch = async () => {
+    document.getElementById(`cau_u_${searchTerm}`).style.backgroundColor = 'yellow';
+    document.getElementById(`cau_${searchTerm}`).scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+    setTimeout(() => {
+      document.getElementById(`cau_u_${searchTerm}`).style.backgroundColor = 'transparent'
+    }, 2000)
+  }
+
+  const cancelUser = async (user) => {
+    const baseUrl = process.env.REACT_APP_BASE_URL
+    if (loading || !user) return
+
+    setLoading(true)
+    var chargebee_subscription_id = user.chargebee_subscription_id
+    if (!chargebee_subscription_id && user?.chargebee_customer_id) {
+      const url = `${process.env.REACT_APP_BASE_URL}/api/subscription_list`
+      let subscription = await axios.post(url,
+        { customer_id: user?.chargebee_customer_id })
+        .then((response) => response.data)
+      console.log(subscription)
+      if (subscription?.id) {
+        chargebee_subscription_id = subscription?.id
+      }
+    }
+    if (!chargebee_subscription_id) {
+      alert("Subscription id not found!");
+      setLoading(false)
+      return
+    }
+
+    console.log(user?.chargebee_customer_id);
+    console.log(chargebee_subscription_id);
+
+    // TODO cancel user in chargebee then supabase
+    let cancelRes = await axios.post(`${baseUrl}/api/cancel_subscription_for_customer`,
+      {
+        subscription_id: chargebee_subscription_id
+      }).catch(err => err)
+    if (cancelRes.status !== 200) {
+      console.log(cancelRes);
+    }
+    // cancel user in supabase
+    const res = await supabase
+      .from("users")
+      .update({ status: 'cancelled' })
+      .eq('email', user?.email)
+      .eq('username', user?.username);
+    if (res?.error) {
+      console.log(res);
+      alert('an error occurred!')
+    }
+
+    // remove the user from list.
+    const userRow = document.getElementById(`cau_${user.username}`)
+    if (userRow) {
+      userRow.style.display = 'none';
+    }
+    
+    setLoading(false)
+  }
+
+  return (<div className='relative h-full overflow-auto'>
+    <form className="fixed z-[999999] w-full bg-white px-4 top-3 left-0 min-h-[30px] border-b-3" onSubmit={(e) => { e.preventDefault(); hangleSearch() }}>
+      <div className="flex items-center px-2 py-4 bg-white border rounded-xl">
+        @<input
+          value={searchTerm}
+          onChange={(e) => { setSearchTerm(e.target.value) }}
+          type="search"
+          className="w-full border-none outline-none"
+          placeholder='Search by username' />
+      </div>
+    </form>
+
+    <div className="min-h-[60px]"></div>
+
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[999999999999]">
+      {loading && <div className="flex items-center justify-center">
+        <img src={LOGO} alt="Loading" className="w-10 h-10 animate-spin" />
+      </div>}
+    </div>
+
+    <table className="mt-[5px] w-full table-auto border-separate border-spacing-y-2">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Account</th>
+          <th>Email</th>
+          <th>Status</th>
+          <th>
+            <div className="text-center">Actions</div>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {users.map((user, index) => {
+          if (!user) {
+            return ("Loading")
+          }
+
+          return (
+            <tr key={`${user?.username}_${index + 1}_row`} id={`cau_${user.username}`} className='rounded-[10px] bg-[#F8F8F8] h-[64px] w-full'>
+              <td>
+                <img src={user?.profile_pic_url} alt="" className="w-[30px] h-[30px] min-w-[30px] min-h-[30px] rounded-full bg-black ml-4" />
+              </td>
+              <td>
+                <div id={`cau_u_${user.username}`} className="flex items-center gap-2">
+                  <div className="relative cursor-pointer max-w-[180px] break-words"
+                    onClick={() => {
+                      copy(user?.username, {
+                        debug: true,
+                        message: 'Press #{key} to copy',
+                      })
+                      setMessage({ sectionName: `username-${user?.username}`, value: 'copied' })
+                      setTimeout(() => {
+                        setMessage({ sectionName: '', value: '' })
+                      }, 1000);
+                    }}
+                  >@{user?.username}
+                    {message.sectionName === `username-${user?.username}` && <div className="absolute font-bold text-black">{message.value}</div>}
+                  </div>
+
+                  <a href={`https://www.instagram.com/${user?.username}/`} target='_blank' rel="noreferrer" >
+                    <FaInstagram size={16} color="red" />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="max-w-[200px] break-words">
+                  <a href={`mailto:${user?.email}`} className="">{user?.email}</a>
+                </div>
+              </td>
+              <td id={`chargebee_status_${user.username}`}></td>
+              <td>
+                <div>
+                  <FaTimes color='red' className="flex items-center justify-center w-full cursor-pointer" onClick={() => {
+                    if (window.confirm("Are you sure you want to set this user to cancel?")) {
+                      cancelUser(user);
+                    }
+                  }} />
+                </div>
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  </div>)
 }
